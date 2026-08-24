7310df8908d15e44ceb98f8434a946b267d6ed6e
Naj <majin1211@gmail.com>
2026-08-24 20:29:49 +0200

    V7

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* PIC — service worker · by Majin */
-const CACHE = "pic-majin-v3";
+const CACHE = "pic-majin-v6";
 const ASSETS = [
   "./", "./index.html", "./styles.css", "./app.js",
   "./intro.js", "./manifest.webmanifest", "./favicon.svg",
